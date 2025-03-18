f510236dd5f7b0f0a88d150a14351da088e9dc27
Olusesi Barry <bolexido192@gmail.com>
2025-03-18 08:20:53 +0100

    fix(user): added auth middleware to route

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -2,11 +2,13 @@ package routes
 
 import (
 	"github.com/BarryDaBee/go_restaurant_management/controllers"
+	"github.com/BarryDaBee/go_restaurant_management/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(route *gin.Engine) {
 	user := route.Group("/user")
+	user.Use(middleware.AuthMiddleware())
 	{
 		user.POST("/register", controllers.Register)
 		user.POST("/login", controllers.Login2)
